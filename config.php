26a57951f0daf315695966a0fce85059a26ef7e0
vincent <thonkghie@gmail.com>
2019-02-25 14:27:14 +0700

    First Package Library SendMe

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+/*
+* Get credential in your inbox mail from email by faspay
+* Version 1.0
+*/
+
+
+$param["development"]["virtual_account"] 	= "9920015021";
+$param["development"]["faspay_key"] 		= "c2b21b0e-e988-4fc8-ae31-39d7177a7186";
+$param["development"]["faspay_secret"] 		= "19c887a3-49af-4087-8e82-58a1875b1452";
+$param["development"]["app_key"] 			= "8e734017-d7c1-49f2-8202-377faaff6a1c";
+$param["development"]["app_secret"] 		= "f26a4e84-cbba-4023-b3d9-40892c5638d4";
+$param["development"]["client_key"] 		= "e5d7e3be-3f44-4da2-918d-3b98db499ed3";
+$param["development"]["client_secret"] 		= "0afd3f5b-b20e-4dc2-8222-d7a571a31c4c";
+$param["development"]["iv"] 				= "faspay2018xAuth";
+
+$param["production"]["virtual_account"] 	= "";
+$param["production"]["faspay_key"] 			= "";
+$param["production"]["faspay_secret"] 		= "";
+$param["production"]["app_key"] 			= "";
+$param["production"]["app_secret"] 			= "";
+$param["production"]["client_key"] 			= "";
+$param["production"]["client_secret"] 		= "";
+$param["production"]["iv"] 					= "";
+
+return $param; 
